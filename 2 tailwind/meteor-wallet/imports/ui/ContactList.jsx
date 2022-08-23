39271222e794e8f308ed2ce9c2557ab8358aeb3e
hanna lam <63362052+hannalam@users.noreply.github.com>
2022-08-23 17:46:05 +0200

    the second lesson

---
+++ 2 tailwind/meteor-wallet/imports/ui/ContactList.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useTracker } from 'meteor/react-meteor-data';
+import { ContactsCollection } from "../api/ContactsCollection";
+
+
+export const ContactList = () => {
+    const contacts = useTracker(() => {
+        return ContactsCollection.find({}).fetch();//Tracker
+    }) 
+    return (
+      <>
+        <h3>Contact List</h3>
+        {contacts.map(contact =>(
+            <li key={contact.email}>{contact.name} - {contact.email}</li>
+        ))}
+
+      </>
+   )
+}
